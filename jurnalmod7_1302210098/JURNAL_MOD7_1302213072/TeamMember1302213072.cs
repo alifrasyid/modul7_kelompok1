db3850f0f2883febb07c1768c4749aab64a4fbaf
PC33-IFLAB <>
2023-04-04 17:14:24 +0700

    menambahkan class_TeamMember1302213072

---
+++ jurnalmod7_1302210098/JURNAL_MOD7_1302213072/TeamMember1302213072.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+namespace JURNAL_MOD7_1302213072;
+public class TeamMember1302213072
+{
+    ListMembers anggota = new ListMembers();
+    public void readJSON()
+    {
+        string text2 = File.ReadAllText("D:\\modul7_kelompok1\\jurnalmod7_1302210098\\JURNAL_MOD7_1302213072\\jurnal7_2_1302213072.json");
+
+        anggota = JsonSerializer.Deserialize<ListMembers>(text2);
+
+        for (int i = 0; i < anggota.members.Count; i++)
+        {
+            Console.WriteLine($"MK {i + 1}: <{anggota.members[i].nim}> < {anggota.members[i].firstName} {anggota.members[i].lastName} {anggota.members[i].age} {anggota.members[i].gender} >");
+        }
+    }
+}
